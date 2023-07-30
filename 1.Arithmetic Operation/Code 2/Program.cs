528b887eadd0543b064b3166b3b6009c61894de2
MOHAMAD NASEER M <75239548+Mohamadnaseer@users.noreply.github.com>
2023-07-31 00:21:34 +0530

    Add files via upload

---
+++ 1.Arithmetic Operation/Code 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ARITHMETIC OPERATION");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("ENTER THE NUMBERS 1:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ENTER THE NUMBERs 2:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("REPORT");
+            Console.WriteLine("------");
+            int c = a + b;
+            Console.WriteLine("ADDITION:" + c);
+            int d = a - b;
+            Console.WriteLine("SUBRACTION:" + d);
+            int e = a * b;
+            Console.WriteLine("MULTIPLICATION:" + e);
+            int f = a / b;
+            Console.WriteLine("DIVISION:" + f);
+            int g = a % b;
+            Console.WriteLine("MODULATION:" + g);
+            Console.ReadLine();
+        }
+    }
+}
