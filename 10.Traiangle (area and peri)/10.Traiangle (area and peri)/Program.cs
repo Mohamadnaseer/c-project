4635eb5a7f780a451144e76fadb59568512a6c42
MOHAMAD NASEER M <75239548+Mohamadnaseer@users.noreply.github.com>
2023-07-31 00:30:35 +0530

    Add files via upload

---
+++ 10.Traiangle (area and peri)/10.Traiangle (area and peri)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Traiangle__area_and_peri_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Triangle\n------------");
+            Console.WriteLine("enter the breadth:");
+            int bre = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter teh height:");
+            int hei = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("value of a");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("value of b");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("value of c");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("report\n----------");
+            int area = (1 *bre * hei)/2;
+            Console.WriteLine("area of traigle:" + area);
+            int peri = a + b + c;
+            Console.WriteLine("perimeter of traigle:" + peri);
+            Console.ReadLine();
+
+        
+        }
+    }
+}
---
+++ 11.student ML/11.student ML/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.student_ML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Student ML");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("REG NO:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("NAME:");
+            string str = Console.ReadLine();
+            Console.WriteLine("mark 1:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("mark 2:");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("mark 3:");
+            int d = Convert.ToInt32(Console.ReadLine());
+            int e = b + c + d;
+            Console.WriteLine("total:" + e);
+            int f = e / 3;
+            Console.WriteLine("percentage:" + f);
+            
+            if (b >= 40 && c >= 40 && d >= 40)
+            {
+                Console.WriteLine("result:pass");
+            }
+            else
+            {
+                Console.WriteLine("result:fail");
+            }
+            if (e >= 250)
+            {
+                Console.WriteLine("grade:1 class");
+            }
+            else if(e >= 200)
+            {
+                Console.WriteLine("grade:2 class");
+            }
+            else if (e >= 150)
+            {
+                Console.WriteLine("grade:3 class");
+            }
+            else
+            {
+                Console.WriteLine("grade:4 class");
+            }
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
