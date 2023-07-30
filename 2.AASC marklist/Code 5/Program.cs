5c146d8b25c47efff78db8a62e8148b94410c57f
MOHAMAD NASEER M <75239548+Mohamadnaseer@users.noreply.github.com>
2023-07-31 00:22:37 +0530

    Add files via upload

---
+++ 2.AASC marklist/Code 5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ACHARIYA ARTS AND SCIENCE COLLEGE");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("REG NO:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("NAME:");
+            string str = Console.ReadLine();
+            Console.WriteLine("mark 1:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("mark 2:");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("mark 3:");
+            int d = Convert.ToInt32(Console.ReadLine());
+            int e = b + c + d;
+            Console.WriteLine("total:" + e);
+            int f = e / 3;
+            Console.WriteLine("percentage:" + f);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 3.Electricity bills/Code 4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GOVERNMENT OF PUDUCHERRY\n ELECTRICITY BILL\n ------------------");
+            Console.WriteLine("EB NO:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("CUSTOMER NAME:");
+            string str = Console.ReadLine();
+            Console.WriteLine("PREVIOUS UNIT:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("CURRENT UNIT:");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("AMOUNT PER UNIT:");
+            int d = Convert.ToInt32(Console.ReadLine());
+
+            int e = b - c;
+            Console.WriteLine("UNIT READING:" + e);
+            int f = c + e;
+            Console.WriteLine("AMOUNT=" + f);
+            int g = e * 100 / 10;
+            Console.WriteLine("GST(10%)=" + g);
+            int h = f + g;
+            Console.WriteLine("TOTAL AMOUNT:" + h);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
